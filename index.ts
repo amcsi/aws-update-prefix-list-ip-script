1fba8191f6d24540fd0c73737b4024e5c5e51779
Attila Szeremi <attila@szeremi.org>
2025-11-10 11:48:54 +0100

    Be able to update multiple regions worth of prefix lists

--- index.ts
+++ index.ts
@@ -8,70 +8,78 @@ import {
 const currentIp = await getMyIp();
 console.log(currentIp, process.env.AWS_ACCESS_KEY_ID);
 
-//TBC need seprate creds that don't need MFA
+const defaultRegion = process.env.AWS_DEFAULT_REGION;
 
-const client = new EC2Client({
-  region: process.env.AWS_DEFAULT_REGION ?? '',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+const prefixListIds = (process.env.PREFIX_LIST_ID ?? '').split(',');
+const regions = (process.env.AWS_REGIONS ?? '').split(',');
+
+await Promise.all(prefixListIds.map(updateForPrefixListId))
+
+async function updateForPrefixListId(prefixListId: string, index: number) {
+  const region = regions[index] ?? defaultRegion;
+
+  const client = new EC2Client({
+    region,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    }
+  })
+
+  const getManagedPrefixListCommand = new GetManagedPrefixListEntriesCommand({
+    PrefixListId: prefixListId,
+  });
+
+
+  const [response, describeManagedPrefixListsResponse] = await Promise.all([ client.send(getManagedPrefixListCommand), client.send(new DescribeManagedPrefixListsCommand({
+    PrefixListIds: [prefixListId]
+  }))]);
+
+  if (!describeManagedPrefixListsResponse.PrefixLists) {
+    console.error({ describeManagedPrefixListsResponse });
+    throw new Error(JSON.stringify(describeManagedPrefixListsResponse));
   }
-})
 
-const prefixListId = process.env.PREFIX_LIST_ID ?? '';
-const getManagedPrefixListCommand = new GetManagedPrefixListEntriesCommand({
-  PrefixListId: prefixListId,
-});
+  const entries = response.Entries;
 
+  if (!entries) {
+    throw new Error("No Entries array found in the response.");
+  }
 
-const [response, describeManagedPrefixListsResponse] = await Promise.all([ client.send(getManagedPrefixListCommand), client.send(new DescribeManagedPrefixListsCommand({
-  PrefixListIds: [prefixListId]
-}))]);
+  const entryToChange = entries.find(v => v.Description === process.env.PREFERRED_DESCRIPTION_TO_CHANGE) ?? entries[0];
 
-if (!describeManagedPrefixListsResponse.PrefixLists) {
-  console.error({ describeManagedPrefixListsResponse });
-  process.exit(1);
+  const newCidr = `${currentIp}/32`;
+
+  if (entryToChange && entryToChange.Cidr === newCidr) {
+    console.info(`${region} IP up to date.`);
+    return;
+  }
+
+  const toRemove: RemovePrefixListEntry[] = [];
+  const toAdd: AddPrefixListEntry[] = [];
+
+  if (entryToChange) {
+    toRemove.push({ Cidr: entryToChange.Cidr });
+  }
+  toAdd.push({
+    Cidr: newCidr,
+    Description: entryToChange?.Description ?? 'Default',
+  });
+
+  const modifyResponse = await client.send(new ModifyManagedPrefixListCommand({
+    PrefixListId: prefixListId,
+    RemoveEntries: toRemove,
+    AddEntries: toAdd,
+    CurrentVersion: describeManagedPrefixListsResponse.PrefixLists[0].Version
+  }));
+
+  if (modifyResponse.$metadata.httpStatusCode !== 200) {
+    console.error(modifyResponse);
+    process.exit(1);
+  }
+
+  console.info(`${region} IP update successful.`);
 }
-
-const entries = response.Entries;
-
-if (!entries) {
-  throw new Error("No Entries array found in the response.");
-}
-
-const entryToChange = entries.find(v => v.Description === process.env.PREFERRED_DESCRIPTION_TO_CHANGE) ?? entries[0];
-
-const newCidr = `${currentIp}/32`;
-
-if (entryToChange && entryToChange.Cidr === newCidr) {
-  console.info("IP up to date.");
-  process.exit(0);
-}
-
-const toRemove: RemovePrefixListEntry[] = [];
-const toAdd: AddPrefixListEntry[] = [];
-
-if (entryToChange) {
-  toRemove.push({ Cidr: entryToChange.Cidr });
-}
-toAdd.push({
-  Cidr: newCidr,
-  Description: entryToChange?.Description ?? 'Default',
-});
-
-const modifyResponse = await client.send(new ModifyManagedPrefixListCommand({
-  PrefixListId: prefixListId,
-  RemoveEntries: toRemove,
-  AddEntries: toAdd,
-  CurrentVersion: describeManagedPrefixListsResponse.PrefixLists[0].Version
-}));
-
-if (modifyResponse.$metadata.httpStatusCode !== 200) {
-  console.error(modifyResponse);
-  process.exit(1);
-}
-
-console.info("IP update successful.");
 
 async function getMyIp() {
   return (await (await fetch('https://api.ipify.org?format=json')).json()).ip;
